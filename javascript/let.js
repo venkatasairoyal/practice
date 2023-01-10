6e6161f15261a97b17007431dede3e9b8dcfb8e0
pulivenkatasai@gmail.com <117662341+venkatasairoyal@users.noreply.github.com>
2023-01-10 15:39:03 +0530

    sai

---
+++ javascript/let.js
@@ -0,0 +1,15 @@
+/* let x="balayya"
+
+let y=0; */
+
+
+var x="balayya"
+var x=0;
+
+
+
+/* in a let key word we should not use same letname */
+
+/* in a var keyword we can use the same let names */
+
+
